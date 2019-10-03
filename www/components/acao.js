67aca55b096db33a9e3d07824e25a5481132e15d
jvitorn <joaoeuno2011@gmail.com>
2019-10-03 10:49:23 +0900

    finalizando cadastro

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,17 +1,30 @@
 // This is a JavaScript file
 $(document).on("click","#enviar",function(){
   var parametros = {
-    "nome" :$("#nome").val(),
-    "senha" :$("#senha").val(),
-    "email":$("#email").val()
+    "nome" :$("#name").val(),
+    "senha" :$("#password").val(),
+    "email": $("#email").val()
   };
 
   $.ajax({
+      //como sera enviado
       type:"post",
-      //url
+       //url onde sera enviado
+      url:"https://jvitorn.000webhostapp.com/connect.php",
+      //o que sera enviado
       data:parametros,
-      sucess:function(data){
+      //Sucess
+      success:function(data){
         navigator.notification.alert(data);
+        //apagando valores
+        $("#name").val("");
+        $("#password").val("");
+        $("#email").val("");
+
+      },
+      //Error
+      error:function(data){
+        navigator.notification.alert("Erro ao cadastrar");
       }
   })
 });
