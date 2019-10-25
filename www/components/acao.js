7961c2bf3e1a6d137cc4203a2690bede191219cd
jvitorn <joaoeuno2011@gmail.com>
2019-10-26 07:19:42 +0900

    versao final

--- www/components/acao.js
+++ www/components/acao.js
@@ -37,16 +37,11 @@ $(document).on('change','#lista',function(){
       let nome = $("#lista option:selected").attr('nome');
       let email = $("#lista option:selected").attr('email');
       let senha = $("#lista option:selected").attr('senha');
-      console.log(nome,email,senha);
         $("#name").val(nome);  
         $("#email").val(email);
         $("#password").val(senha); 
      });
-     $('.editar').click(function(){
-       $("#name").attr('readyonly',false);
-       $("#email").attr('readyonly',false);
-       $("#password").attr('readyonly',false);
-     });
+    
 function listar(){
   $.ajax({
       //como sera enviado
@@ -62,7 +57,7 @@ function listar(){
           itemlista += "<option value='"+dados.codigo+"' nome='"+dados.nome+"' email='"+dados.email+"' senha='"+dados.senha+"'>"+dados.nome+"</option>";
        });
        $("#lista").html(itemlista);
-       console.log(itemlista);
+       
       },
       //Error
       error:function(data){
@@ -70,4 +65,102 @@ function listar(){
       }
   })
 }
+$(document).on('click','.editar',function(){
+  $("#name").attr('readonly',false);
+  $("#email").attr('readonly',false);
+  console.log('entrou');
+  $("#password").attr('readonly',false);
+});
+$(document).on('click','.salvar',function(){
+  let codigo = $("#lista option:selected").val();
+  let nome = $("#name").val();
+  let senha = $("#password").val();
+  let email = $("#email").val();
+   let parametros = {
+    "codigo" :codigo,
+    "nome":nome,
+    "email":email,
+    "senha" : senha
+  };
+   $.ajax({
+      //como sera enviado
+      type:"post",
+       //url onde sera enviado
+      url:"https://jvitorn.000webhostapp.com/alterar.php",
+      //o que sera enviado
+      data:parametros,
+      //Sucess
+      success:function(data){
+        Swal.fire({
+          type: 'success',
+          title: data,
+           confirmButtonText:'OK',
+          timer: 1500
+        }).then((result) => {
+            if (result.value) {
+              location.reload();
+            }
+        });
+        
 
+      },
+      //Error
+      error:function(data){
+         Swal.fire({
+          type: 'error',
+          title: 'Erro ao excluir',
+          showConfirmButton: false,
+          timer: 1500
+        })
+        
+      }
+  })
+});
+$(document).on('click','.excluir',function(){
+  let codigo = $("#lista option:selected").val();
+   let parametros = {
+    "codigo" :codigo
+  };
+   $.ajax({
+      //como sera enviado
+      type:"post",
+       //url onde sera enviado
+      url:"https://jvitorn.000webhostapp.com/delete.php",
+      //o que sera enviado
+      data:parametros,
+      //Sucess
+      success:function(data){
+        Swal.fire({
+          type: 'success',
+          title: data,
+          showConfirmButton: false,
+          timer: 1500
+        }).then((result) => {
+            if (result.value) {
+              location.reload();
+            }
+        });
+        
+
+      },
+      //Error
+      error:function(data){
+         Swal.fire({
+          type: 'error',
+          title: 'Erro ao excluir',
+          showConfirmButton: false,
+          timer: 1500
+        }).then((result) => {
+            if (result.value) {
+              location.reload();
+            }
+        });
+        
+      }
+  })
+});
+$(document).on('click','.cancelar',function(){
+  $("#name").attr('readonly',true);
+  $("#email").attr('readonly',true);
+  $("#password").attr('readonly',true);
+});
